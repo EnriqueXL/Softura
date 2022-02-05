eb8d7ebf6498f284c9c36d61880fce6e56ea8b28
Luis Enrique <luiswetowkn@gmail.com>
2022-02-04 18:13:34 -0600

    login/register

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController, NavController, MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  /* Atributo */
+  formularioLogin: FormGroup; 
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController,
+    public navCtrl:NavController,
+    public menuCtrl:MenuController) { 
+
+    /* Iniciamos formulario */
+    this.formularioLogin = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required)
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  async ingresar(){
+    var f = this.formularioLogin.value;
+    
+    var usuario = JSON.parse(localStorage.getItem('usuario'));
+    if (usuario.nombre == f.nombre && usuario.password == f.password){
+      console.log('Ingresado');
+      localStorage.setItem('Ingresado','true');
+      this.navCtrl.navigateRoot('inicio');
+    }else{
+      const alert = await this.alertController.create({
+        header:'Datos incorrectos',
+         message: 'Los datos no son correctos',
+         buttons: ['Aceptar']
+       });
+   
+       await alert.present();
+
+
+    }
+  }
+
+  ionViewDidEnter() {
+    // el menú de la raíz izquierda debe estar deshabilitado en esta página
+    this.menuCtrl.enable(false);
+    }
+    ionViewWillLeave () {
+    // habilita el menú raíz izquierdo al salir de esta página
+      this.menuCtrl.enable (true);
+    }
+
+}
---
+++ src/app/registro/registro.page.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.page.html',
+  styleUrls: ['./registro.page.scss'],
+})
+export class RegistroPage implements OnInit {
+   
+  /* Atributo */
+   formularioRegistro: FormGroup; 
+
+  constructor(public fb: FormBuilder,
+    public alertController:AlertController,
+    public navCtrl:NavController,
+    public menuCtrl: MenuController) {
+    /* Iniciamos formulario */
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required),
+      'confirmacionPassword': new FormControl("",Validators.required)
+    });
+   }
+
+  ngOnInit() {
+  }
+
+  /* Evento */
+  async guardar(){
+    var f = this.formularioRegistro.value;
+
+    if (this.formularioRegistro.invalid) {
+      const alert = await this.alertController.create({
+       header:'Datos incompletos',
+        message: 'Todos los campos deben estar llenos',
+        buttons: ['Aceptar']
+      });
+  
+      await alert.present();
+      /* Evito que la función continue */
+      return;
+    }
+
+    var usuario ={
+      nombre: f.nombre,
+      password: f.password
+    }
+    localStorage.setItem('usuario',JSON.stringify(usuario));
+    console.log("x",usuario);
+    
+  }
+
+  ionViewDidEnter() {
+    // el menú de la raíz izquierda debe estar deshabilitado en esta página
+    this.menuCtrl.enable(false);
+    }
+    ionViewWillLeave () {
+    // habilita el menú raíz izquierdo al salir de esta página
+      this.menuCtrl.enable (true);
+    }
+}
